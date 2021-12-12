ce9cbfd7d53f11994509ad8613235d7056b2fa1c
LeeSeungjae00 <seungjae266789@gmail.com>
2021-12-12 22:31:00 +0900

    header animation add mobile ui fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,45 @@
 import React, { useState, useEffect } from 'react'
+import {
+    useTransition,
+    useSpring,
+    useChain,
+    config,
+    animated,
+    useSpringRef,
+} from '@react-spring/web'
 import MenuIcon from '@mui/icons-material/Menu';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import style from './Header.module.css'
-import { Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 
 
 export default function Header() {
     const [scrollPosition, setScrollPosition] = useState(window.screenX)
-    const [menuHover, setMenuHover] = useState(false);
+    const [open, set] = useState(false)
+
+    const springApi = useSpringRef()
+    const { size, ...rest } = useSpring({
+        ref: springApi,
+        config: config.stiff,
+        to: {
+            size: open ? '50vw' : '3vw',
+        },
+    })
+
+    const transApi = useSpringRef()
+    const transition = useTransition(open ? 1 : [], {
+        ref: transApi,
+        trail: 400,
+        from: { opacity: 0, scale: 0 },
+        enter: { opacity: 1, scale: 1 },
+        leave: { opacity: 0, scale: 0 },
+    })
+
+    useChain(open ? [springApi, transApi] : [transApi, springApi], [
+        0,
+        open ? 0.6 : 0.1,
+    ])
+
     const updateScroll = () => {
         setScrollPosition(window.scrollY || document.documentElement.scrollTop);
     }
@@ -18,29 +50,35 @@ export default function Header() {
     return (
         <header className={scrollPosition < 100 ? style.header : style.changeHeader}>
             <div className={scrollPosition < 100 ? style.headerContent : style.changeHeaderContent}>
-                <div className={style.menuButton} 
-                onMouseOver={() => {if(!menuHover) setMenuHover(true);}} 
-                onMouseLeave={() => {if(menuHover) setMenuHover(false);}}
+                <animated.div
+                    style={{ ...rest, width: size }}
+                    className={style.menuButton}
+                    onMouseOver={() => { if (!open) set(true); }}
+                    onMouseLeave={() => { if (open) set(false); }}
                 >
-                    {!menuHover ? 
-                    <MenuIcon sx={{ color: 'rgb(155,155,152)', fontSize: "3rem" }} ></MenuIcon> : 
-                    <ArrowForwardIcon sx={{ color: 'rgb(155,155,152)', fontSize: "3rem" }} ></ArrowForwardIcon>}
-                    {menuHover && <>
-                        <Link activeClass="active" className="main" to="main" spy={true} smooth={true} duration={500} >
-                        <div className = {style.menu}>🏠 HOME</div>
-                        </Link>
-                        <Link activeClass="active" className="about" to="about" spy={true} smooth={true} duration={500} >
-                        <div className = {style.menu}>👨 ABOUT ME</div>
-                        </Link>
-                        <Link activeClass="active" className="skill" to="skill" spy={true} smooth={true} duration={500} >
-                        <div className = {style.menu}>📚 SKILL</div>
-                        </Link>
-                        <Link activeClass="active" className="project" to="project" spy={true} smooth={true} duration={500} >
-                        <div className = {style.menu}>👨‍💻 PROJECTS</div>
-                        </Link>
-                    </>}
+                    {!open ?
+                        <MenuIcon sx={{ color: 'rgb(155,155,152)', fontSize: "3rem" }} ></MenuIcon> :
+                        <ArrowForwardIcon sx={{ color: 'rgb(155,155,152)', fontSize: "3rem" }} ></ArrowForwardIcon>}
+                    {transition((style) => (
+                        <animated.div
+                            style={{ ...style, display: 'flex', width: '100%', justifyContent: 'space-around' }}
+                        >
 
-                </div>
+                            <Link activeClass="active" className="main" to="main" spy={true} smooth={true} duration={500} >
+                                <div className={style.menu}>🏠 HOME</div>
+                            </Link>
+                            <Link activeClass="active" className="about" to="about" spy={true} smooth={true} duration={500} >
+                                <div className={style.menu}>👨 ABOUT ME</div>
+                            </Link>
+                            <Link activeClass="active" className="skill" to="skill" spy={true} smooth={true} duration={500} >
+                                <div className={style.menu}>📚 SKILL</div>
+                            </Link>
+                            <Link activeClass="active" className="project" to="project" spy={true} smooth={true} duration={500} >
+                                <div className={style.menu}>👨‍💻 PROJECTS</div>
+                            </Link>
+                        </animated.div>
+                    ))}
+                </animated.div>
                 <h2 className={style.headerTitle}>SJ's Portfolio</h2>
                 <div></div>
             </div>
