57a429aa7663bec34ed7236d03a1e51712471681
LeeSeungjae00 <seungjae266789@gmail.com>
2021-12-13 16:56:01 +0900

    remove warring

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     useTransition,
     useSpring,
@@ -50,7 +50,7 @@ export default function Header() {
     useEffect(() => {
         window.addEventListener('scroll', updateScroll);
         window.innerWidth < 900 ? setMoblieView(true) : setMoblieView(false);
-    })
+    } , [setMoblieView])
 
     return (
         <header className={scrollPosition < 100 ? style.header : style.changeHeader}>
